c0fcc339bed2d1e4dad07adbe6419262dd619b8a
MihirPetkar108 <mihirpetkar2006@gmail.com>
2025-07-09 01:57:11 +0530

    Added images

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./landing_page/home/HomePage";
+import Signup from "./landing_page/signup/Signup";
+import About from "./landing_page/about/AboutPage";
+import Product from "./landing_page/products/ProductsPage";
+import Pricing from "./landing_page/pricing/PricingPage";
+import Support from "./landing_page/support/SupportPage";
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <div>
-            <h1>Hello!</h1>
-        </div>
-    </React.StrictMode>
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/product" element={<Product />}></Route>
+            <Route path="/pricing" element={<Pricing />}></Route>
+            <Route path="/support" element={<Support />}></Route>
+        </Routes>
+        <Footer />
+    </BrowserRouter>
 );
