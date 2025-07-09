90131dc8cbfeeb31348b311fbe3c75970131fecf
MihirPetkar108 <mihirpetkar2006@gmail.com>
2025-07-09 19:56:10 +0530

    Frontend complete

--- src/landing_page/OpenAccount.js
+++ src/landing_page/OpenAccount.js
@@ -4,11 +4,6 @@ function OpenAccount() {
     return (
         <div className="container p-5 mb-5">
             <div className="row text-center">
-                <img
-                    src="media/images/homeHero.png"
-                    alt="Hero Image"
-                    className="mb-5"
-                />
                 <h1 className="mt-3 mb-4">Open a Zerodha account</h1>
                 <p className="mb-5">
                     Modern platforms and apps, ₹0 investments, and flat ₹20
